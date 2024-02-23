0b1fb1e2ebd82953b4d5998f9d1b5efc35e0d363
DevMdAsif <mdasifullah456@gmail.com>
2024-02-23 11:25:45 +0600

    first problem solved

---
+++ condition/first-problem.py
@@ -0,0 +1,19 @@
+# 1 . student gpa : , A-(number<40) , A(nummber<80) , A+(number<100)
+
+number = 101
+
+if number<33:
+  print('fail')
+
+elif number<40:
+    print('-A')
+
+elif number<80:
+    print('A')
+
+
+elif number<100:
+    print('A')
+
+else :
+    print("invalde number")
